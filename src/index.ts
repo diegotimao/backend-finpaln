bdac0359bd3933489fad121ab8b022cb4760b56f
Diego <diegotimao1104@gmail.com>
2023-06-23 16:20:43 -0300

    feat: Adicionando middleares de erros com a lib assync-erros

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response, NextFunction } from "express";
 import { StatusCodes } from "http-status-codes";
+import 'express-async-errors';
+import UserRoutes from "./routes/user.routes";
 
 const app = express();
 
@@ -11,6 +13,29 @@ app.post("/login", (req: Request, res: Response) => {
   const user = req.body;
   return res.status(StatusCodes.OK).json(user)
 });
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  const { name, message, details } = err as any;
+  console.log(`name: ${name}`);
+
+  switch (name) {
+    case 'ValidationError':
+      res.status(400).json({message: details[0].message});
+      break;
+    case 'NotFoundError':
+      res.status(404).json({ message });
+      break;
+    case 'ConflictError':
+      res.status(409).json({ message });
+      break;
+    default:
+      console.log(err);
+      res.sendStatus(500);
+  }
+  next();
+});
+
+app.use(UserRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is runing at http://localhost:${PORT}`)
