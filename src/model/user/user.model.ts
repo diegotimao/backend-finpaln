34b281f3a6b98e2ee3c274ee3b7552e95f67444b
Diego <diegotimao1104@gmail.com>
2023-06-24 03:53:22 -0300

    feat: Refatorando model de usuário

--- src/model/user/user.model.ts
+++ src/model/user/user.model.ts
@@ -1,6 +1,5 @@
 import { Pool, ResultSetHeader } from 'mysql2/promise';
 import User from '../../interfaces/user.interface';
-import { timeStamp } from 'console';
 
 export default class UserModel {
   public connection: Pool;
