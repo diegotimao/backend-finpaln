fce8dd381f9d2bfb47d903e139b6b320e88f7d6d
Diego <diegotimao1104@gmail.com>
2023-06-24 03:54:23 -0300

    feat: Criando arquivo de rotas de usuário

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,6 +1,15 @@
-import { Router, Request, Response } from "express";
-import { StatusCodes } from "http-status-codes";
+import { Router,Response, Request } from 'express';
+import UserController from '../controllers/user/user.controller';
+import { StatusCodes } from 'http-status-codes';
 
 const router = Router();
+const userController = new UserController();
+
+router.get('/users', userController.getAll);
+
+router.post("/login", (req: Request, res: Response) => {
+  const user = req.body;
+  return res.status(StatusCodes.OK).json(user)
+});
 
 export default router;
