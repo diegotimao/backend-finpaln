caa57a223b2ab26edf662774e4b56bee791d280b
Diego <diegotimao1104@gmail.com>
2023-06-23 21:00:33 -0300

    feat: Implementando o método que cria um novo usuário no banco de dados

--- src/model/user/user.model.ts
+++ src/model/user/user.model.ts
@@ -1,5 +1,6 @@
 import { Pool, ResultSetHeader } from 'mysql2/promise';
 import User from '../../interfaces/user.interface';
+import { timeStamp } from 'console';
 
 export default class UserModel {
   public connection: Pool;
@@ -12,5 +13,15 @@ export default class UserModel {
     const result = await this.connection.execute('SELECT * FROM users');
     const [rows] = result;
     return rows as User[];
+  }
+
+  public async createUser(user: User): Promise<User> {
+    const { name, hash_password } = user;
+
+    const result = await this.connection.execute<ResultSetHeader>(
+      'INSERT INTO users (name, hash_password, create_at) VALUES (?,?,?)', [name, hash_password, new Date()]);
+    const [dataInserted] = result;
+    const { insertId } = dataInserted;
+    return { id: insertId, ...user};
   }
 }
