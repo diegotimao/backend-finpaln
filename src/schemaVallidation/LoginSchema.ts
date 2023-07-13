f719cc3ee81fd96b128597eda49afc8744595f02
Diego <diegotimao1104@gmail.com>
2023-07-13 14:50:29 -0300

    feat: Schema de login validação zod

---
+++ src/schemaVallidation/LoginSchema.ts
@@ -0,0 +1,29 @@
+import z, { AnyZodObject } from 'zod';
+import { NextFunction, Response, Request } from 'express';
+
+const LoginSchema = z.object({
+  body: z.object({
+    email: z.string().email("O e-mail precisa ser válido."),
+    hash_password: z.string().min(6, "A senha precisa ter no minímo 6 caractres.")
+  })
+});
+
+type LoginSchema = z.infer<typeof LoginSchema>;
+
+const ValidateLogin = (schema: AnyZodObject) => async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    await schema.parseAsync({
+      body: req.body,
+      query: req.query,
+      params: req.params
+    });
+    return next()
+  } catch(err) {
+    if (err instanceof z.ZodError) {
+      const resultError = err.issues;
+      return res.status(400).json({message: resultError[0].message});
+    };
+  }
+}
+
+export { ValidateLogin, LoginSchema };
