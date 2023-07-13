372969d93de93e3b95a7e19f6f4feca7de8a3ecf
Diego <diegotimao1104@gmail.com>
2023-07-13 14:55:36 -0300

    feat: Criando a rota de register

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,12 +1,14 @@
-import { Router,Response, Request } from 'express';
+import { Router } from 'express';
 import UserController from '../controllers/user/user.controller';
-import { StatusCodes } from 'http-status-codes';
+import { ValidateLogin, LoginSchema } from '../schemaVallidation/LoginSchema';
+import { ValidationRegister, RegisterSchema } from '../schemaVallidation/RegisterSchema';
 
 const router = Router();
 const userController = new UserController();
 
 router.get('/users', userController.getAll);
 
-router.post("/login", userController.login);
+router.post("/user/login", ValidateLogin(LoginSchema), userController.login);
+router.post("/user/register", ValidationRegister(RegisterSchema), userController.register);
 
 export default router;
