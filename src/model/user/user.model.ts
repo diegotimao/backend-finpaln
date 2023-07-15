b4217a68ef84101b75ff75157b74775f0a82aa75
Diego <diegotimao1104@gmail.com>
2023-07-15 03:34:30 -0300

    feat: Implementando o  getIsers na model

--- src/model/user/user.model.ts
+++ src/model/user/user.model.ts
@@ -1,23 +1,15 @@
-import { Pool, ResultSetHeader } from 'mysql2/promise';
-import User from '../../interfaces/user.interface';
+import { PrismaClient, User } from '@prisma/client';
+// import User from '../../interfaces/user.interface';
 
 export default class UserModel {
-  public connection: Pool;
+  public prisma: PrismaClient;
 
-  constructor(connection: Pool) {
-    this.connection = connection;
+  constructor() {
+    this.prisma = new PrismaClient();
   };
 
-  public async login(email: string): Promise<User> {
-    const result = await this.connection.execute('SELECT * FROM users WHERE email = (?)', [email]);
-    const [rows] = result;
-    const [user] = rows as User[];
-    return user as User;
-  };
-
-  public async getAll(): Promise<User[]> {
-    const result = await this.connection.execute('SELECT * FROM users');
-    const [rows] = result;
-    return rows as User[];
-  };
+  async getUsers(): Promise<User[]> {
+    const users = await this.prisma.user.findMany();
+    return users;
+  }
 };
