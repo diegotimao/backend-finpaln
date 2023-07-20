98cb36cf6960b05934a67b90cb2632100c10772a
Diego <diegotimao1104@gmail.com>
2023-07-20 20:18:15 -0300

    feat: Implementando a rota de login

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import UserController from '../controllers/user/user.controller';
-import { ValidateLogin, LoginSchema } from '../schemaVallidation/LoginSchema';
 import { ValidationRegister, RegisterSchema } from '../schemaVallidation/RegisterSchema';
+import { LoginSchema, ValidateLogin } from "../schemaVallidation/LoginSchema";
 
 const router = Router();
 const userController = new UserController();
 
 router.post("/user/register", ValidationRegister(RegisterSchema), userController.createUser);
+router.post("/user/login", ValidateLogin(LoginSchema), userController.login);
 
 export default router;
