9e7010c3eff6e9c5d4cb23d7cdbca477c307c1a8
Diego <diegotimao1104@gmail.com>
2023-07-20 20:17:53 -0300

    feat: Implementando o método login na class UserCrontoller

--- src/controllers/user/user.controller.ts
+++ src/controllers/user/user.controller.ts
@@ -6,12 +6,32 @@ class UserController {
   constructor(private userService = new UserService()) {}
 
   public createUser = async (req: Request, res: Response) => {
+    const {name, email, password} = req.body;  
     try {
-      const {name, email, password} = req.body;
       const result = await this.userService.createUser(name, email, password);
-      return res.status(StatusCodes.OK).json({token: result})
+      return res.status(StatusCodes.CREATED).json({token: result})
     } catch (error) {
-      res.status(500).json({ error: "Usuário já existente." });
+      if (error instanceof Error) {
+        return res.status(StatusCodes.NOT_FOUND).json({ message: error.message});
+      }
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Erro ao processar a requisição.' });
+    }
+  }
+
+  public login = async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    try {
+      const result = await this.userService.login(email, password);
+      
+      if (result === null) {
+        return res.status(StatusCodes.UNAUTHORIZED).json({ message: 'Credenciais inválidas.' });
+      }
+      return res.status(StatusCodes.OK).json({ token: result})
+    } catch (error) {
+      if (error instanceof Error) {
+        return res.status(StatusCodes.NOT_FOUND).json({ message: error.message});
+      }
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Erro ao processar a requisição.' });
     }
   }
 }
