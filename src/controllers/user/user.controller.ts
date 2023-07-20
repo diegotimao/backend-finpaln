a50e2d720c8868ce6635ae62dfede8286d929f64
Diego <diegotimao1104@gmail.com>
2023-07-20 12:49:15 -0300

    feat: Refatorando a classe UserCorontoller

--- src/controllers/user/user.controller.ts
+++ src/controllers/user/user.controller.ts
@@ -5,33 +5,14 @@ import UserService from "../../services/users/user.service";
 class UserController {
   constructor(private userService = new UserService()) {}
 
-  public login = async (req: Request, res: Response) => {
+  public createUser = async (req: Request, res: Response) => {
     try {
-      const user = await this.userService.login(req.body);
-      return res.status(StatusCodes.OK).json(user)
+      const {name, email, password} = req.body;
+      const result = await this.userService.createUser(name, email, password);
+      return res.status(StatusCodes.OK).json({token: result})
     } catch (error) {
-      if (error instanceof TypeError) {
-        console.log(error.message)
-        console.log(error.stack)
-      }
+      res.status(500).json({ error: "Usuário já existente." });
     }
-  }
-
-  public register = async (req: Request, res: Response) => {
-    try {
-      const result = await this.userService.register(req.body);
-      return res.status(StatusCodes.OK).json(result)
-    } catch (error) {
-      if (error instanceof TypeError) {
-        console.log(error.message)
-        console.log(error.stack)
-      }
-    }
-  }
-
-  public getAll = async (_req: Request, res: Response) => {
-    const users = await this.userService.getAll();
-    return res.status(StatusCodes.OK).json(users);
   }
 }
 
