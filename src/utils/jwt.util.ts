68b6331c92a181e0fd2e7c9d0010730aca9dfc07
Diego <diegotimao1104@gmail.com>
2023-07-20 12:27:31 -0300

    fix: Corrigindo as funções de token

--- src/utils/jwt.util.ts
+++ src/utils/jwt.util.ts
@@ -5,19 +5,15 @@ const secret = process.env.JWT_SECRET || 'secret';
 type TokenPyloud = {
   id: number,
   email: string,
+  name: string,
 }
 
-function sign(pyload: TokenPyloud): string {
+export function generateToken(pyload: TokenPyloud): string {
   const token = jwt.sign(pyload, secret);
   return token;
 }
 
-function verifyToken(token: string): TokenPyloud {
+export function verifyToken(token: string): TokenPyloud {
   const data = jwt.verify(token, secret) as TokenPyloud;
   return data;
 }
-
-export default {
-  sign,
-  verifyToken
-};
